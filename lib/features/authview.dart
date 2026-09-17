a1006d0394e57702309a8170c7fe779705b5a974
Hanaa Edreis <hanaaedreis7@gmail.com>
2026-09-17 17:23:04 +0300

    Third commit

---
+++ lib/features/authview.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AuthLayout extends StatelessWidget{
+  final Widget child;
+  const AuthLayout({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      
+
+
+    );
+  }
+
+}
--- lib/features/signup.dart
+++ lib/features/signup.dart
@@ -13,39 +13,8 @@ class _SignupPage extends State<SignupPage> {
       backgroundColor: Color(0xffFFFFFF),
       body: Column(
         children: [
-          Padding(
-            padding: EdgeInsets.only(
-              top: MediaQuery.sizeOf(context).height*0.1,
-              left: MediaQuery.sizeOf(context).width*0.05
-            ),
-            child:Text.rich(
-                TextSpan(
-                  children:[
-                    TextSpan(
-                      text: 'Let\'s ',
-                      style: TextStyle(
-                        color: Color(0xff00a1a9),
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic
-                  )
-                ),
-                TextSpan(
-                  text: 'Create your profile',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.w500
-                  )
-                )
-              ] )
-          )
-          ),
-
-          SizedBox(height: MediaQuery.sizeOf(context).height * 0.02,),
-
           
-        ],
-
+        ]
       ),
 
     );
